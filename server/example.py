60854293364c8d9be873ac6cfe330246321d7325
charlieweinberger <364charliebrown@gmail.com>
2024-06-22 16:18:34 -0700

    Removed my api key from git

--- server/example.py
+++ server/example.py
@@ -6,8 +6,8 @@ import asyncio
 async def main() -> None:
     
     load_dotenv()
-    HUME_API_KEY = os.getenv("HUME_API_KEY")
-    client = HumeVoiceClient(HUME_API_KEY)
+    CHARLIE_HUME_API_KEY = os.getenv("CHARLIE_HUME_API_KEY")
+    client = HumeVoiceClient(CHARLIE_HUME_API_KEY)
 
     # Start streaming EVI over your device's microphone and speakers 
     async with client.connect() as socket:
